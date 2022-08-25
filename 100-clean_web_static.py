5e34770aeb75a73ab56653a42dd4613c285b270d
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-25 14:41:31 +1000

    fix: Made the code more universal

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,8 +4,8 @@ script (based on the file 3-deploy_web_static.py)
 that deletes out-of-date archives,
 using the function do_clean:
 """
+import os
 from fabric.api import *
-
 env.hosts = ['52.204.151.72', '3.90.36.230']
 env.user = "ubuntu"
 env.key_filename = ['./my_ssh_private_key']
@@ -14,14 +14,21 @@ env.key_filename = ['./my_ssh_private_key']
 def do_clean(number=0):
     """cleans up relases folders """
 
-    number = int(number)
-
-    if number == 0:
-        number = 2
+    if int(number) == 0:
+        number = 1
     else:
-        number += 1
+        number = int(number)
 
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    serverPath = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(
-        serverPath, number))
+    localFiles = sorted(os.listdir("versions"))
+    for i in range(number):
+        localFiles.pop()
+    with lcd("versions"):
+        for file in localFiles:
+            local("rm ./{}".format(file))
+
+    with cd("/data/web_static/releases"):
+        serverFiles = run("ls -tr").split()
+        for i in range(number):
+            serverFiles.pop()
+        for file in serverFiles:
+            run("rm -rf ./{}".format(file))
