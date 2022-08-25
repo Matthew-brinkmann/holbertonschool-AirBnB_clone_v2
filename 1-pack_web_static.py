579dee9584ae8716d9a9b50cc1da5b607eb786aa
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-25 11:14:45 +1000

    build: add task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo,
+using the function do_pack
+"""
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """generates a .tgz pack"""
+    fileCreateDate = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('mkdir -p versions')
+    newFileName = "versions/web_static_{}.tgz".format(fileCreateDate)
+    resultOfCreate = local("tar -cvzf {} web_static".format(newFileName))
+    if resultOfCreate.failed:
+        return None
+    else:
+        return resultOfCreate
